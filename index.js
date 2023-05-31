2f19f2af60be465c04bebc3852d6214f41421fa3
Anna <34191811+404unf@users.noreply.github.com>
2023-05-30 22:18:06 -0700

    Array filters

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers,matchThis) {
+    return drivers.filter(driver => driver.toLowerCase() === matchThis.toLowerCase())
+}
+
+function fuzzyMatch(drivers, matchThis) {
+    return drivers.filter(driver => driver.startsWith(matchThis))
+}
+
+function matchName(drivers, matchThis) {
+    return drivers.filter(driver => driver.name === matchThis)
+}
